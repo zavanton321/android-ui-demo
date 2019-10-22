71383328d2748308db9d146c2cd2b186fb9c7213
Anton Zaviyalov <zavanton123@gmail.com>
2019-10-22 16:14:32 +0300

    linear and frame

--- app/src/main/java/com/example/uidemo/MainActivity.kt
+++ app/src/main/java/com/example/uidemo/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.uidemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val demo: LinearLayout? = null
+
+        val count = demo?.childCount ?: 0
+
+        for (i in 0..count) {
+            val result = demo?.getChildAt(i)
+        }
     }
 }
