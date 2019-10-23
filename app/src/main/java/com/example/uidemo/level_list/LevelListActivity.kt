4505805d574665c1eaeb5eeff57c22286eec31df
Anton Zaviyalov <zavanton123@gmail.com>
2019-10-23 17:20:08 +0300

    level list

---
+++ app/src/main/java/com/example/uidemo/level_list/LevelListActivity.kt
@@ -0,0 +1,13 @@
+package com.example.uidemo.level_list
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.uidemo.R
+
+class LevelListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_level_list)
+    }
+}
