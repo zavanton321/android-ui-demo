6ead32b6f4f9b5010948f9420173f65436a9cb8e
Anton Zaviyalov <zavanton123@gmail.com>
2019-10-23 16:59:43 +0300

    state list

--- app/src/main/java/com/example/uidemo/state_list/StateListActivity.kt
+++ app/src/main/java/com/example/uidemo/state_list/StateListActivity.kt
@@ -1,13 +1,13 @@
-package com.example.uidemo.ui
+package com.example.uidemo.state_list
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.uidemo.R
 
-class DemoActivity : AppCompatActivity() {
+class StateListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_demo)
+        setContentView(R.layout.activity_state_list)
     }
 }
