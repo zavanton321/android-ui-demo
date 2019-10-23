bc256db15172fb380f7333bfc8d3df553cd99bd0
Anton Zaviyalov <zavanton123@gmail.com>
2019-10-23 17:56:23 +0300

    InsetDrawable ok

---
+++ app/src/main/java/com/example/uidemo/inset/InsetActivity.kt
@@ -0,0 +1,14 @@
+package com.example.uidemo.inset
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.uidemo.R
+
+class InsetActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inset)
+
+    }
+}
