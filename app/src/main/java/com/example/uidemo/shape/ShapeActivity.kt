5823837c9cc8559ee5b427c3f3c1daeb5a1f62a6
Anton Zaviyalov <zavanton123@gmail.com>
2019-10-24 10:47:33 +0300

    ShapeDrawable

---
+++ app/src/main/java/com/example/uidemo/shape/ShapeActivity.kt
@@ -0,0 +1,13 @@
+package com.example.uidemo.shape
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.uidemo.R
+
+class ShapeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shape)
+    }
+}
